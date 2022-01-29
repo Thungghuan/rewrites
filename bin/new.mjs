986182284a118078673d277f57341d8afdb295c9
Thungghuan <1092887426@qq.com>
2022-01-29 14:07:34 +0800

    feat: remove the `git init` command in the subfolder

--- bin/new.mjs
+++ bin/new.mjs
@@ -18,11 +18,9 @@ let answer = await question('Do you want to install dependencies? [Y/n] ')
 if (answer.toLowerCase() !== 'n') {
   await $`cd ${project} && npm install`
   await $`code ${project}`
-  await $`git init`
 } else {
   console.log('Type commands below to start your project yourself:\n')
 
   console.log(chalk.yellow(`cd ${project}`))
   console.log(chalk.yellow(`npm install`))
-  console.log(chalk.yellow(`git init`))
 }
