6d5fe1100a7c11de16a4fef7ce7643080f5a3b29
Thungghuan <1092887426@qq.com>
2022-02-04 15:32:45 +0800

    feat[express]: update some methods

--- express/index.ts
+++ express/index.ts
@@ -1,6 +1,11 @@
 import express from './src/application'
+// import express from 'express'
 
 const app = express()
+
+app.get('/', (req, res) => {
+  console.log('Get /')
+})
 
 app.listen(3000, () => {
   console.log('Server listen on port 3000')
--- express/src/application.ts
+++ express/src/application.ts
@@ -4,11 +4,30 @@ const createExpress = (): Application => {
   return new Application()
 }
 
+type HTTPRequestMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'OPTION' | 'HEAD'
+type RequestDataHandler = {
+  [K in HTTPRequestMethod]?: any[]
+}
+
 class Application {
-  app: http.Server
+  app: http.Server | null = null
+  requestDataHandlers: RequestDataHandler = {}
+
+  get(path: string, handler: (req: any, res: any) => void) {
+    if (!this.requestDataHandlers['GET']) this.requestDataHandlers['GET'] = []
+
+    this.requestDataHandlers['GET'].push({
+      path,
+      handler
+    })
+  }
 
   listen(port: number, ...args: any[]) {
-    this.app = http.createServer()
+    this.app = http.createServer((req, res) => {
+      this.requestDataHandlers[req.method!.toUpperCase() as HTTPRequestMethod]!
+        .filter((handler: any) => handler.path === req.url)
+        .map((handler: any) => handler.handler())
+    })
 
     this.app.listen(port, ...args)
   }
