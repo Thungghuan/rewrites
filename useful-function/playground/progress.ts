46b20116723fcaea8634effe40fb9df16c31cd95
Thungghuan <1092887426@qq.com>
2022-05-04 12:12:56 +0800

    feat: update

--- useful-function/playground/progress.ts
+++ useful-function/playground/progress.ts
@@ -2,7 +2,8 @@ import { throttle } from './utils'
 
 const progressRange: HTMLInputElement =
   document.querySelector('.progress-range')!
-const progressNum: HTMLDivElement = document.querySelector('.progress-num')!
+const progressNum: NodeListOf<HTMLDivElement> =
+  document.querySelectorAll('.progress-num')!
 
 const leftHalf: HTMLDivElement = document.querySelector(
   '.ring-progress .left-half'
@@ -12,11 +13,16 @@ const rightHalf: HTMLDivElement = document.querySelector(
 )!
 const ringMask: HTMLDivElement = document.querySelector('.ring-progress .mask')!
 
+const progress2: HTMLDivElement = document.querySelector('.ring-progress2')!
+
+
 const inputHandelr = (e: InputEvent) => {
   const target = e.target as HTMLInputElement
-  progressNum.textContent = target.value
+  progressNum.forEach((e) => (e.textContent = target.value))
 
   const value = +target.value
+
+  progress2.style.backgroundImage = `conic-gradient(blue ${value}%, transparent 0)`
 
   if (value <= 50) {
     rightHalf.style.transform = `rotate(${value * 3.6 - 135}deg)`
